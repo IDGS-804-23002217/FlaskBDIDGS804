c51175ed499404cbe68cbc3e9223304104a01a8a
tarae1500 <maythe@balleza.ws>
2026-02-20 18:47:53 -0600

    detalles 20/02/26

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from config import DevelopmentConfig
 from flask import g
 import forms
 
+
 from models import db
 from models import Alumnos
 
 app= Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf= CSRFProtect()
-
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -25,10 +25,33 @@ def index():
 	alumno=Alumnos.query.all()
 	return render_template("index.html",form=create_form,alumno=alumno)
 
-
-@app.route("/alumnos")
+@app.route('/alumnos', methods=['GET', 'POST'])
 def alumnos():
-	return render_template("Alumnos.html")
+    alumno_class=forms.UserForm(request.form)
+    if request.method=='POST':
+        alum=Alumnos(nombre=alumno_class.nombre.data,
+               apaterno=alumno_class.apaterno.data,
+               email=alumno_class.email.data)
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("Alumnos.html", form=alumno_class)
+ 
+@app.route("/detalles", methods=['GET','POST'])
+def detalles():
+	create_form=forms.UserForm(request.form)
+	if request.method=='GET':
+		id=request.args.get('id')
+		# select * from alumnos where id == id 
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		id=request.args.get('id')
+		nombre=alum1.nombre
+		apaterno=alum1.nombre
+		email=alum1.email
+
+	return render_template('detalles.html', id=id, nombre=nombre, apaterno=apaterno, email=email)
+
+
 
 if __name__ == '__main__':
 	csrf.init_app(app)
